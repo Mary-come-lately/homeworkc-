da6d0ada54f3f8dfb4b4bfef17609f8572529425
el-fadel <el-fadel@mail.ru>
2019-02-19 11:59:13 -0500

    initial import

---
+++ Task2.cpp
@@ -0,0 +1,89 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+void task1()
+{
+	cout << " To be" << endl;
+	cout << "     or not" << endl;
+	cout << " \t    to be!" << endl;
+	cout << "     \\Shakespeare\\ " << endl;
+	system("pause");
+}
+
+void task2()
+{
+	cout << "Name \t   : \"The war and the peace\" " << endl;
+	cout << "Avtor \t   : L.N.Tolstoj" << endl;
+	cout << "Izdatelstvo: Piter" << endl;
+	cout << "Pages \t   : 500" << endl;
+	system("pause");
+}
+
+void task3()
+{
+	cout << "Name       - PP-12-1" << endl;
+	cout << "Creator    - student" << endl;
+	cout << "Size       - 50MB" << endl;
+	cout << "Path       - D:\\Academy\\PP-12-1" << endl;
+	system("pause");
+}
+
+void task4()
+{
+	cout << "City ------------  Ukraine" << endl;
+	cout << "Street ----------  \"Karl Marks\" avenue" << endl;
+	cout << "House ----------   101" << endl;
+	cout << "Flat ------------- 45" << endl;
+	system("pause");
+}
+
+void task5()
+{
+	cout << "Vasa                      FirstName" << endl;
+	cout << "Pupkin                    LastName" << endl;
+	cout << "25                        Age" << endl;
+	cout << "student of                Status \ncomputer academy \"STEP\" " << endl;
+	system("pause");
+}
+
+void task6()
+{
+	cout << " +---------------------------------------------------------------------------------------------+" << endl;
+	cout << "|                                 Vrema dna                                                     |" << endl;
+	cout << " +-------------------------------+----------------------------+--------------------------------+" << endl;
+	cout << "|           Utro                  |          Den               |                Vecher          |" << endl;
+	cout << " +-------------------------------+----------------------------+-------------------------------=+" << endl;
+	system("pause");
+}
+
+void task7()
+{
+	cout << "   mm       mm                           iii " << endl;
+	cout << "mm    mm mm    mm    aaaa  a   rr  rr         nn  nn      aaaa  a" << endl;
+	cout << "mm      mm     mm  aaa   aaa   rrr   rr  iii  nn    nn  aaa   aaa" << endl;
+	cout << "mm             mm  aaa   aaa   rr        iii  nn    nn  aaa   aaa" << endl;
+	cout << "mm             mm    aaaa  aa  rr        iii  nn    nn     aaaa   aa" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "eeeeeeeee  ll         fffff                     dd            ll" << endl;
+	cout << "eee        ll        ff   ff    aaaa  a      dd dd   eeeeee   ll" << endl;
+	cout << "eeeeeeeee  ll  --  ffffff     aaa   aaa   dd    dd  eeeeee    ll" << endl;
+	cout << "eee        ll        ff       aaa   aaa   dd    dd  ee        ll" << endl;
+	cout << "eeeeeeeee  ll        ff         aaaa  aa   ddd  dd   eeeee    ll" << endl;
+}
+
+int main()
+{
+	task1();
+	task2();
+	task3();
+	task4();
+	task5();
+	task6();
+	task7();
+	return 0;
+}
